20509ec2d6435c77e0a40ac08d516d28757bd575
muestea <dev0019@naver.com>
2022-08-16 17:31:47 +0900

    menuEffect1

---
+++ src/routes/pages/HappyDay.svelte
@@ -0,0 +1,120 @@
+<script>
+    import {onMount} from 'svelte'
+    let eleContainer;
+    const RANGE = 100;
+    
+
+    const circle = async() => {
+        const amout = 100;
+        let i = 0; 
+        while(i < amout){
+            if(eleContainer){
+                let ele = document.createElement('i')
+                eleContainer.appendChild(ele);
+                //console.log(ele);
+
+                let posX = Math.floor(Math.random() * eleContainer.innerWidth);
+                ele.style.left = posX + 'px';
+
+                let size = parseInt(Math.random() * 250);
+                ele.style.width = `${(10 + size)}px`;
+                ele.style.height = `${10 + size}px`;
+                
+               
+            } 
+            i++;
+        }
+    };
+    const getStyle = (i) => {
+        let size = parseInt(Math.random() * 250);
+        let posX = Math.floor(Math.random() * eleContainer.innerWidth);
+        let delay = Math.random() * -20;
+        let duration = Math.random() * 10;
+        let style =  `width:${(10 + size)}px;` + 
+            `height:${10 + size}px;`  + 
+            `left:${posX}px` + 
+            `animationDelay:${delay}s` + 
+            `animationDuration:${5 + duration}s` + 
+            `webkitBoxShadow:0 30px 50px #0002`;
+        return style;
+    }
+
+    $: innerWidth = 0;
+    
+    onMount(()=>{
+       
+    });
+
+
+</script>
+<svelte:window bind:innerWidth/>
+
+<div class="container" bind:this={eleContainer}>
+    <h2>
+        Happy <br>
+        Independence <br>
+        Day
+    </h2>
+    {#each Array.from(Array(100+1).keys()).slice(1) as i }
+        <i style={getStyle(i)}></i>                    
+    {/each}
+</div>
+
+<style lang="scss">
+    .container{
+        position: relative;
+        height: 100vh;
+        overflow: hidden;
+        background: #fff;
+
+        h2{
+            position: absolute;
+            top: 50%;
+            width: 100%;
+            transform: translateY(-50%);
+            font-size: 12vw;
+            line-height: 1em;
+            font-weight: 800;
+            text-align: center;
+            color: #333;
+            text-transform: uppercase;
+
+            
+        }
+
+        i{
+            position: absolute;
+            display: block;
+            background: #000;
+            border-radius: 50%;
+            animation: animate 5s linear infinite;
+        }
+        i:nth-child(5n+1){
+            background: #fff;
+        }
+        i:nth-child(5n+2){
+            background: #ffb03c;
+        }
+        i:nth-child(5n+3){
+            background: #fff;
+            border: #ff9933;
+        }
+        i:nth-child(5n+4){
+            background: #2bd11b;
+            border: #ff9933;
+        }
+        i:nth-child(5n+5){
+            background: #fff;
+            border: #2bd11b;
+        }
+    }
+
+    @keyframes animate {
+        0%{
+            transform: translateY(calc(100vh + 350px));
+        }
+        100%{
+            transform: translateY(-300px);
+        }
+    }
+</style>
---
+++ src/routes/pages/MenuEffect.svelte
@@ -0,0 +1,101 @@
+<script>
+
+</script>
+
+<div class="container">
+    <ul>
+        <li style={"--i:6"}><a href="#">Home</a></li>
+        <li style={"--i:5"}><a href="#">About</a></li>
+        <li style={"--i:4"}><a href="#">About</a></li>
+        <li style={"--i:3"}><a href="#">Services</a></li>
+        <li style={"--i:2"}><a href="#">Our Team</a></li>
+        <li style={"--i:1"}><a href="#">Content</a></li>
+    </ul>
+</div>
+
+
+<style lang="scss">
+    $--li-color: #3e3f46;
+    $--a-color: #999;
+
+    .container{
+        font-family: 'Oswald', sans-serif;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        background: #434750;
+
+        ul{
+            position: relative;
+            transform: skewY(-15deg);
+            
+
+            li{
+                position: relative;
+                list-style: none;
+                width: 200px;
+                background: $--li-color;
+                padding: 15px;
+                z-index: var(--i);
+                transition: 0.5s;
+
+                &:hover{
+                    background: #33a3ee;
+                    transform: translateX(-50px);
+                    
+                    &::before{
+                        background: #1f5378;
+                    }
+
+                    &::after{
+                        background: #2982b9;
+                    }
+                    
+                    a{
+                        color: #fff;
+                    }
+                }
+
+                &::before{
+                    content: '';
+                    position: absolute;
+                    top: 0;
+                    left: -40px;
+                    width: 40px;
+                    height: 100%;
+                    background: #2e3133;
+                    transform-origin: right;
+                    transform: skewY(45deg);
+                    transition: 0.5s;
+                }
+                &::after{
+                    content: '';
+                    position: absolute;
+                    top: -40px;
+                    left: 0;
+                    width: 100%;
+                    height: 40px;
+                    background: #35383e;
+                    transform-origin: bottom;
+                    transform: skewX(45deg);
+                }
+
+                &:last-child::after{
+                    box-shadow: -120px 120px 20px rgba(0,0,0,0.5);
+                }
+
+
+                a{
+                    text-decoration: none;
+                    color: $--a-color;
+                    display: block;
+                    text-transform: uppercase;
+                    letter-spacing: 0.05em;
+                    transition: 0.5s;
+                }
+            }
+        }
+
+    }
+</style>
