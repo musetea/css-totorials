ae87bd48d629f0b65de8ef220b7ec5aed5e15a3b
muestea <dev0019@naver.com>
2022-08-17 00:26:47 +0900

    base64File

---
+++ src/routes/pages/ImgBase64.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount} from 'svelte';
+    let eleFile;
+
+
+    onMount(()=>{
+        eleFile.addEventListener('change', (e) => {
+            // console.log(e);
+            const file = eleFile.files[0];
+            const reader = new FileReader();
+            reader.addEventListener('load', ()=>{
+                console.log(reader.result);
+
+            });
+            reader.readAsDataURL(file);
+        })
+    })
+
+</script>
+
+<div class="container">
+    <input type="file" bind:this={eleFile}>
+</div>
